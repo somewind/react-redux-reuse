3a3f90ef91d8241ff72d35913bf904d641351d9f
somewind <neverstopsteps@gmail.com>
2016-10-09 11:40:41 +0800

    fix import bug

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * Created by maoshuchen on 9/28/2016.
  */
 import connect from './connect'
-import bindActionCreator from './bindActionCreators'
+import bindActionCreators from './bindActionCreators'
 
 export {
   connect,
-  bindActionCreator
+  bindActionCreators
 }
